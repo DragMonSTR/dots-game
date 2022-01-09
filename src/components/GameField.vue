c7f7fb4618cc835acc8e3923bfcef2eb1ed47c82
VladyslavOleksenko <vlad8oleksenko1107@gmail.com>
2022-01-09 19:46:40 +0200

    Create GameField component

---
+++ src/components/GameField.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="game-field">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GameField"
+}
+</script>
+
+<style scoped>
+.game-field {
+  background-color: #eaeaea;
+}
+</style>

59dbf0f27ac6d1ac4010b337ae63a0dcee77ffef
VladyslavOleksenko <vlad8oleksenko1107@gmail.com>
2022-01-09 19:46:57 +0200

    use GameField component in Game view

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,13 +1,28 @@
 <template>
-It is a game view
+  <div class="game">
+    <GameField class="game__game-field"/>
+  </div>
 </template>
 
 <script>
+import GameField from "@/components/GameField";
 export default {
-  name: "Game"
+  name: "Game",
+  components: {GameField}
 }
 </script>
 
 <style scoped>
+.game {
+  height: 100vh;
 
+  display: flex;
+  justify-content: center;
+}
+
+
+.game__game-field {
+  width: 100vh;
+  height: 100vh;
+}
 </style>

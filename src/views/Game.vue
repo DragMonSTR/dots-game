e2b2980d976e0fa6221d99fde3084cefa89311c7
VladyslavOleksenko <vlad8oleksenko1107@gmail.com>
2022-01-10 19:21:26 +0200

    little refactor

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -5,7 +5,9 @@
 </template>
 
 <script>
-import GameField from "@/components/GameField";
+import GameField from "@/components/GameField"
+
+
 export default {
   name: "Game",
   components: {GameField}

c6009b14f6b70c2de8a24e13f8b9699ed367c18d
VladyslavOleksenko <vlad8oleksenko1107@gmail.com>
2022-01-10 19:19:06 +0200

    Create Cell component

---
+++ src/components/Cell.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="cell">
+    <div
+      class="cell__main-circle"
+      :style="{'background-color': getCellColor(cellInfo)}"
+    >
+      <div
+        class="cell__dot"
+        v-for="i in cellInfo.dotsNumber"
+        :key="i"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Game from "@/assets/gameLogic/game";
+
+export default {
+  name: "Cell",
+  props: {
+    cellInfo: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    getCellColor(cellInfo) {
+      if (cellInfo.playerIndex === -1) {
+        return "#fff"
+      }
+      const player = Game.playerArray[cellInfo.playerIndex]
+      return player.color
+    }
+  }
+}
+</script>
+
+<style scoped>
+.cell__main-circle {
+  width: 65%;
+  height: 65%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+
+  border-radius: 50%;
+
+  transition: transform .2s ease;
+  cursor: pointer;
+}
+
+.cell__main-circle:hover {
+  transform: scale(1.1);
+}
+
+
+.cell__dot {
+  width: 20%;
+  height: 20%;
+  margin-right: 20%;
+  transform: translateX(50%);
+
+  background-color: #fff;
+  border-radius: 50%;
+}
+</style>

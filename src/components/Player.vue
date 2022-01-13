485d79ebbe79deea58d21103cb7e8a0332835d83
VladyslavOleksenko <vlad8oleksenko1107@gmail.com>
2022-01-13 21:13:13 +0200

    Create Player component

---
+++ src/components/Player.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="player">
+    Player
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Player",
+  props: {
+    player: {
+      type: Object,
+      required: true
+    }
+  },
+  mounted() {
+    console.log(this.player)
+  }
+}
+</script>
+
+<style scoped>
+.player {
+  background-color: #aaa;
+}
+</style>

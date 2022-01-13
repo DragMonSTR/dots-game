84d6e5061b8ee2920862c6312c3c4a62e008ac1c
VladyslavOleksenko <vlad8oleksenko1107@gmail.com>
2022-01-13 21:13:27 +0200

    Create Statistics component

---
+++ src/components/Statistics.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="statistics">
+    <div class="player-list">
+      <Player
+        v-for="(player, i) in players"
+        :key="i"
+        :player="player"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Player from "@/components/Player"
+import Game from "@/assets/gameLogic/game"
+
+export default {
+  name: "Statistics",
+  components: {Player},
+  computed: {
+    players() {
+      return Game.playerArray.value
+    }
+  }
+}
+</script>
+
+<style scoped>
+.statistics {
+  display: flex;
+  align-items: center;
+
+  background-color: #ffe;
+}
+
+
+.player-list {
+  width: 100%;
+}
+</style>

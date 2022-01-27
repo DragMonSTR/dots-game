96f5153e79dbf7a69e1384f0e126949d54d0567e
VladyslavOleksenko <vlad8oleksenko1107@gmail.com>
2022-01-27 16:05:07 +0200

    Create store strings module

---
+++ src/store/strings.js
@@ -0,0 +1,21 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    startGame: {
+      en: "start game",
+      ru: "играть"
+    },
+    rules: {
+      en: "rules",
+      ru: "правила"
+    },
+    language: {
+      en: "language",
+      ru: "язык"
+    },
+    mainMenu: {
+      en: "main menu",
+      ru: "главное меню"
+    }
+  })
+}

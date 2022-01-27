332168c7ebf8deb94b1170468e56a23860f118dc
VladyslavOleksenko <vlad8oleksenko1107@gmail.com>
2022-01-27 17:00:01 +0200

    Update Button component
    - change component name to MyButton
    - little refactor

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="button">
-    <img
-      class="button__icon"
-      :src="iconFileName"
-      alt=""
-    >
-    <p
-      class="button__text"
-    >{{ text }}</p>
+  <div class="my-button">
+    <img class="icon"
+         :src="iconFileName"
+         alt="">
+
+    <p class="text">
+      {{ text }}
+    </p>
   </div>
 </template>
 
@@ -26,15 +25,16 @@ export default {
   },
   computed: {
     iconFileName() {
-      return require(`../assets/icons/${this.iconName}.svg`)
+      return require(`@/assets/img/icons/${this.iconName}.svg`)
     }
   }
 }
 </script>
 
 <style scoped>
-.button {
-  padding: 20px 50px;
+.my-button {
+  height: 100px;
+  padding: 0 50px;
 
   display: inline-flex;
   align-items: center;
@@ -46,18 +46,18 @@ export default {
   cursor: pointer;
 }
 
-.button:hover {
+.my-button:hover {
   background-color: #e3e3c9;
 }
 
 
-.button__icon {
+.icon {
   width: 50px;
   height: 50px;
   margin: 0 20px 0 0;
 }
 
-.button__text {
+.text {
   font-size: 50px;
   text-align: center;
 

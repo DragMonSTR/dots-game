043a3bf5b6d7d4bd0ae652c9b8955d1987dfb8d6
VladyslavOleksenko <vlad8oleksenko1107@gmail.com>
2022-01-27 16:05:17 +0200

    Connect store modules to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
+import language from "@/store/language"
+import strings from "@/store/strings"
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
+  getters: {
+    getStringResource: (state, getters) => stringName => {
+      const language = state.language.currentLanguage
+      return state.strings[stringName][language]
+    }
   },
   modules: {
+    language,
+    strings
   }
 })
